b8b8ee1a7f296c962b0be90a8b0bfc2482e27ea7
Carlos Gabriel <gomescarlosleite@gmail.com>
2024-06-29 22:35:32 -0300

    add faq accordion files

---
+++ Frontend Mentor - FAQ Accordion/README.md
@@ -0,0 +1,4 @@
+# Page finished:
+<a href="https://carlsgabriel.github.io/frontendMentorFAQAccordion/">![carlsgabriel github io_frontendMentorFAQAccordion_](https://github.com/carlsgabriel/frontendMentorFAQAccordion/assets/171501592/590fccf9-7ef6-47ed-b04d-1a03bb075215)</a>
+
+## Frontend Mentor - Challenge: <a href="https://www.frontendmentor.io/solutions/html-css-and-javascript-WcxkSpIS1q">FAQ Accordion</a>
---
+++ Frontend Mentor - FAQ Accordion/script.js
@@ -0,0 +1,35 @@
+const buttons = document.querySelectorAll(".faq-ask img");
+const asks = document.querySelectorAll(".faq-ask span");
+const responses = document.querySelectorAll(".faq-response");
+
+
+
+buttons.forEach((value, index) => {
+    value.addEventListener("click", () => {
+        let button = buttons[index];
+        let resp = responses[index];
+
+        if (resp.classList.contains("hidden")) {
+            resp.classList.toggle("hidden");
+            button.setAttribute("src", "assets/images/icon-minus.svg");
+        } else {
+            resp.classList.toggle("hidden");
+            button.setAttribute("src", "assets/images/icon-plus.svg");
+        }
+    })
+});
+
+asks.forEach((value, index) => {
+    value.addEventListener("click", () => {
+        let button = buttons[index];
+        let resp = responses[index];
+
+        if (resp.classList.contains("hidden")) {
+            resp.classList.toggle("hidden");
+            button.setAttribute("src", "assets/images/icon-minus.svg");
+        } else {
+            resp.classList.toggle("hidden");
+            button.setAttribute("src", "assets/images/icon-plus.svg");
+        }
+    })
+});
